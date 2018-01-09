dabc8a48055711aced20e3e3fa3a612eaabc4d8e
Smart-AI <weizhidaoshuo@gmail.com>
2018-01-09 16:33:13 +0800

    update
    Signed-off-by: Smart-AI <weizhidaoshuo@gmail.com>

---
+++ SAAlgorithm/SAAlgorithm/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SAAlgorithm
+//
+//  Created by Smart-AI on 09/01/2018.
+//  Copyright © 2018 Smart-AI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
